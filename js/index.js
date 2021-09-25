6fdd2d06ddb681a69aa3ff40e5cad88b8c4e6458
viraj0786 <virajmalviya.pali@gmail.com>
2021-09-26 02:51:43 +0530

    add html in document daynamically

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+import { myteam } from "../data/data.js"
+
+
 $(document).ready(function(){
     $('.slider').slick({
         dots: true,
@@ -11,3 +14,19 @@ $(document).ready(function(){
         initialSlide: 2
     });
   });
+
+
+
+//render recipe on th ui
+var data = document.querySelector(".slider");
+myteam.map(function(teaminfo){
+    let teamhtml = `     
+    <div class="card">
+    <div class="box">
+    <img src="${teaminfo.team_image}" alt="">
+    <div class="text">${teaminfo.teamates}</div>
+    <p>${teaminfo.position}</p>
+    </div>
+</div>` 
+data.insertAdjacentHTML("beforeend",teamhtml)
+});
